f0de42d0d636b643703344f60eaf939e0f4f8169
MFPDISC <productions.of.onset@gmail.com>
2026-04-01 21:37:58 +0200

    fix: added basePath for github pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // Required for GitHub Pages sub-directory hosting
+  basePath: '/Mr.Mystery',
   images: {
     unoptimized: true,
   },
